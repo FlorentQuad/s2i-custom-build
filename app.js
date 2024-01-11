f5fa3d213d47b4df1ad885f1e4cee46834395725
florent <florent@localhost.localdomain>
2024-01-11 20:20:09 +0100

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello From Node Server\n');
+  res.send('Hello From Custom Node Server\n');
 });
 
 app.listen(8080, function () {
